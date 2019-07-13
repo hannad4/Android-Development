7eed8eadfe2bbba5f0320f3969da0b2bb9e6902c
hannad4 <hannad4@tcnj.edu>
2019-07-13 18:17:47 -0400

    Adding extra functionality to DiceRoller (you can roll, increment, or reset)

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,28 +15,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        val countButton: Button = findViewById(R.id.reset_button)
+        val resetButton: Button = findViewById(R.id.reset_button)
+        val countUpButton: Button = findViewById(R.id.countUp_button)
         rollButton.setOnClickListener { rollDice() }
-        countButton.setOnClickListener { reset() }
+        resetButton.setOnClickListener { reset() }
+        countUpButton.setOnClickListener { countUp() }
 
     }
 
-    public fun rollDice() {
+    private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
 
     }
 
-
     private fun reset() {
         val resultText: TextView = findViewById(R.id.result_text)
-        if (resultText.text == "0") {
-            Toast.makeText(this,"Dice is already reset",Toast.LENGTH_SHORT).show();
+        if (resultText.text == "1") {
+            Toast.makeText(this,"Dice is already reset",Toast.LENGTH_SHORT).show()
         }
         else {
-            resultText.text = "0"
+            resultText.text = "1"
         }
 
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        if (resultText.text == "6") {
+            Toast.makeText(this, "Dice already maxed out", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            val increment = resultText.text.toString().toInt() + 1
+            resultText.text = increment.toString()
+        }
     }
 }
