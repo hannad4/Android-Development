f9b8e317fc6527dd988f6f856ef72851de67d59e
hannad4 <hannad4@tcnj.edu>
2019-07-19 11:21:22 -0400

    Added second dice. However, code is very repetitive and not efficient. Further optomization can be done

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,7 +13,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage : ImageView          // Defining bunch of promises because constant ID indexing is taxing
+    lateinit var diceImage1 : ImageView          // Defining bunch of promises because constant ID indexing is taxing
+    lateinit var diceImage2 : ImageView
     lateinit var rollButton: Button
     lateinit var resetButton: Button
     lateinit var countUpButton: Button
@@ -24,7 +25,8 @@ class MainActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
         rollButton = findViewById(R.id.roll_button)
         resetButton = findViewById(R.id.reset_button)
         countUpButton = findViewById(R.id.countUp_button)
@@ -35,9 +37,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1     // Die needs to be between 0-5 (add 1 to make it "normal" 1-6)
-
-        val drawableResource = when (randomInt) {
+        val randomInt1 = Random().nextInt(6) + 1     // Die needs to be between 0-5 (add 1 to make it "normal" 1-6)
+        val randomInt2 = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -46,22 +48,45 @@ class MainActivity : AppCompatActivity() {
             6 -> R.drawable.dice_6
             else -> R.drawable.empty_dice       // Only using empty vector image as fail safe (see comment below as well)
         }
-        diceImage.setImageResource(drawableResource)
+        diceImage1.setImageResource(drawableResource)
 
-        when (randomInt) {          // Tags are used to keep track of current image source (no getImageSrc() exists)
-            1 -> diceImage.tag = "R.drawable.dice_1"
-            2 -> diceImage.tag = "R.drawable.dice_2"
-            3 -> diceImage.tag = "R.drawable.dice_3"
-            4 -> diceImage.tag = "R.drawable.dice_4"
-            5 -> diceImage.tag = "R.drawable.dice_5"
-            6 -> diceImage.tag = "R.drawable.dice_6"
-            else -> diceImage.tag = "R.drawable.dice_empty"     // The empty vector xml seems confusing. Only going to use it as fail safe
+        val drawableResource2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice       // Only using empty vector image as fail safe (see comment below as well)
+        }
+        diceImage2.setImageResource(drawableResource2)
+
+        when (randomInt1) {          // Tags are used to keep track of current image source (no getImageSrc() exists)
+            1 -> diceImage1.tag = "R.drawable.dice_1"
+            2 -> diceImage1.tag = "R.drawable.dice_2"
+            3 -> diceImage1.tag = "R.drawable.dice_3"
+            4 -> diceImage1.tag = "R.drawable.dice_4"
+            5 -> diceImage1.tag = "R.drawable.dice_5"
+            6 -> diceImage1.tag = "R.drawable.dice_6"
+            else -> diceImage1.tag = "R.drawable.dice_empty"     // The empty vector xml seems confusing. Only going to use it as fail safe
+        }
+
+        when (randomInt2) {          // Tags are used to keep track of current image source (no getImageSrc() exists)
+            1 -> diceImage2.tag = "R.drawable.dice_1"
+            2 -> diceImage2.tag = "R.drawable.dice_2"
+            3 -> diceImage2.tag = "R.drawable.dice_3"
+            4 -> diceImage2.tag = "R.drawable.dice_4"
+            5 -> diceImage2.tag = "R.drawable.dice_5"
+            6 -> diceImage2.tag = "R.drawable.dice_6"
+            else -> diceImage2.tag = "R.drawable.dice_empty"     // The empty vector xml seems confusing. Only going to use it as fail safe
         }
     }
 
     private fun reset() {
-        diceImage.setImageResource(R.drawable.dice_1)
-        diceImage.tag = "R.drawable.dice_1"                 // Image resource is changed to first die, tag should keep track of that
+        diceImage1.setImageResource(R.drawable.dice_1)
+        diceImage1.tag = "R.drawable.dice_1"                 // Image resource is changed to first die, tag should keep track of that
+        diceImage2.setImageResource(R.drawable.dice_1)
+        diceImage2.tag = "R.drawable.dice_1"
 
         resetToast = Toast.makeText(this,"Dice reset", Toast.LENGTH_SHORT)      // Notify user of reset
         resetToast.setGravity(Gravity.BOTTOM, 0, 24)      // Different android models place this in weird locations. Way to fix?
@@ -69,7 +94,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun countUp() {
-        val countUp = when (diceImage.tag) {        // Use tag to know current source so next number can be displayed
+        val countUp = when (diceImage1.tag) {        // Use tag to know current source so next number can be displayed
             "R.drawable.dice_1" -> R.drawable.dice_2
             "R.drawable.dice_2" -> R.drawable.dice_3
             "R.drawable.dice_3" -> R.drawable.dice_4
@@ -78,9 +103,30 @@ class MainActivity : AppCompatActivity() {
             "R.drawable.dice_6" -> R.drawable.dice_6   // Dice is maxed out. Can't count past 6 on dice!
             else -> R.drawable.empty_dice
         }
-        diceImage.setImageResource(countUp)        // Set the new source so that updated die is shown to user
+        diceImage1.setImageResource(countUp)        // Set the new source so that updated die is shown to user
 
-        diceImage.tag = when (diceImage.tag) {      // Image resource was just updated. tag needs to keep up by updating itself too
+        diceImage1.tag = when (diceImage1.tag) {      // Image resource was just updated. tag needs to keep up by updating itself too
+            "R.drawable.dice_1" -> "R.drawable.dice_2"
+            "R.drawable.dice_2" -> "R.drawable.dice_3"
+            "R.drawable.dice_3" -> "R.drawable.dice_4"
+            "R.drawable.dice_4" -> "R.drawable.dice_5"
+            "R.drawable.dice_5" -> "R.drawable.dice_6"
+            "R.drawable.dice_6" -> "R.drawable.dice_6" // Dice is maxed out. Can't count past 6 on a dice!
+            else -> "R.drawable.dice_empty"
+        }
+
+        val countUp2 = when (diceImage2.tag) {        // Use tag to know current source so next number can be displayed
+            "R.drawable.dice_1" -> R.drawable.dice_2
+            "R.drawable.dice_2" -> R.drawable.dice_3
+            "R.drawable.dice_3" -> R.drawable.dice_4
+            "R.drawable.dice_4" -> R.drawable.dice_5
+            "R.drawable.dice_5" -> R.drawable.dice_6
+            "R.drawable.dice_6" -> R.drawable.dice_6   // Dice is maxed out. Can't count past 6 on dice!
+            else -> R.drawable.empty_dice
+        }
+        diceImage2.setImageResource(countUp2)        // Set the new source so that updated die is shown to user
+
+        diceImage2.tag = when (diceImage2.tag) {      // Image resource was just updated. tag needs to keep up by updating itself too
             "R.drawable.dice_1" -> "R.drawable.dice_2"
             "R.drawable.dice_2" -> "R.drawable.dice_3"
             "R.drawable.dice_3" -> "R.drawable.dice_4"
